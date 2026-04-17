0445a4f63cdf2c3194d2912b57fda3cae1b6a995
Soawii <65302714+Soawii@users.noreply.github.com>
2026-04-17 14:28:21 +0300

    New changes

--- db/setup.js
+++ db/setup.js
@@ -1,11 +1,7 @@
 const pool = require("./pool");
 
 const SQL = `
-    DROP TABLE IF EXISTS items_categories;
-    DROP TABLE IF EXISTS items;
-    DROP TABLE IF EXISTS categories;
-
-    CREATE TABLE items (
+    CREATE TABLE IF NOT EXISTS items (
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         title VARCHAR(255) NOT NULL,
         description VARCHAR(255),
@@ -13,12 +9,12 @@ const SQL = `
         amount INTEGER NOT NULL
     );
 
-    CREATE TABLE categories (
+    CREATE TABLE IF NOT EXISTS categories (
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         title VARCHAR(255)
     );
 
-    CREATE TABLE items_categories (
+    CREATE TABLE IF NOT EXISTS items_categories (
         item_id INTEGER,
         category_id INTEGER,
         CONSTRAINT fk_item
@@ -27,23 +23,10 @@ const SQL = `
             FOREIGN KEY (category_id) REFERENCES categories(id),
         PRIMARY KEY (item_id, category_id)
     );
-
-    INSERT INTO items (title, description, price, amount) 
-    VALUES ('item1', 'best item', 1000.0, 10), 
-    ('item2', 'good item', 2000.0, 5),
-    ('item3', 'mid item', 3000.0, 1);
-
-    INSERT INTO categories (title) 
-    VALUES ('cat1'), ('cat2'), ('cat3');
-
-    INSERT INTO items_categories (item_id, category_id)
-    VALUES (1, 1), (1, 2), (1, 3), (2, 2), (3, 3);
 `;
 
-async function main() {
+async function db_setup() {
     console.log("Connecting to  db...");
     await pool.query(SQL);
     console.log("Connection success!");
 }
-
-main();
